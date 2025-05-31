19fe357028fd531738fc00e2ad765dfeda7c8d29
Guy <paike.cool@gmail.com>
2025-05-31 11:42:06 +0200

    Create includes.hpp

---
+++ Driver/includes.hpp
@@ -0,0 +1,23 @@
+#include "Windows.h"
+#include <Windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <TlHelp32.h>
+#include "ntdef.h"
+#include "ks.h"
+#include "stdio.h"
+#include <thread>
+#include "MemoryManager.hpp"
+#include <winternl.h>
+#include <atomic>
+#include <filesystem>
+#include "ntdefs.h"
+#include <tchar.h>
+#include <stdlib.h>
+#include <psapi.h>
+#include <cstdint>
+#include <shared_mutex>
+
+#pragma comment(lib, "ntdll.lib")
